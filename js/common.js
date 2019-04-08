507f9b515b781ff9c6e6c0464e62d5fa1987b99b
Sergey Alekseev <keny510@mail.ru>
2019-04-08 23:18:02 +0300

    editing

--- js/common.js
+++ js/common.js
@@ -1,46 +1,57 @@
 
-// const _store = {
-//     pickedUser : '',
-//     tokens : []
-// }
-
-// const store = new Proxy(_store, {
-//     get(target, prop) {
-//         return target[prop];
-//     },
-//     set(target, prop, value) {
-
-//         let prev = target[prop];
-//         target[prop] = value;
-
-//         if (prop == "tokens") {
-//             setSelectOptions(value);
-//         }
-
-//         if (prop == "pickedUser" && prev) {
-//             loadAndRenderDataUsers();
-//         }
-
-//         return true;
-//     }
-// });
-
 
 let timeout = null;
 let lastReq = null;
 
+js.get('#search').focus();
+
 js.get('#search').addEventListener('input', function(event) {
+    setState('loading');
     if (timeout) clearTimeout(timeout);
     timeout = setTimeout(() => {
         search(event.target.value);
-        lastReq = event.target.value;
         timeout = null;
     }, 200); 
 });
 
+search('Гарри поттер');
+
+js.get('#searched_result').addEventListener('click', function(event) {
+    setFocus(event.target);
+});
+
+window.addEventListener('keyup', function(event) {
+    if (event.code == 'Tab') {
+        let el = event.target;
+        let range = document.createRange();
+        let sel = window.getSelection();
+        range.setStart(el.childNodes[0], event.target.innerText.length);
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+        setFocus(el);
+    }
+});
+
+function setFocus(target) {
+    // удаляем пред.
+    setDefaultStatus();
+
+    // добавляем новый статус
+    let closestSearchedItem = target.closest(`.searched_item`);
+    let wrapperStatus = closestSearchedItem.querySelector('.wrapper_status');
+    js.attr(wrapperStatus, 'state', 'save');
+}
+
+function setDefaultStatus() {
+    let pr_wrapper_status = js.get('#searched_result').querySelector(`.wrapper_status:not([state="hidden"]`);
+    // ставим дефолтный статус hidden
+    if (pr_wrapper_status) js.attr(pr_wrapper_status, 'state', 'hidden');
+}
 
 async function search(text) {
     setState('loading');
+    lastReq = text;
     const searchData = await getSearchData(text);
     renderSearchData(searchData, text);
 
@@ -86,9 +97,16 @@ function renderSearchData(searchData, text) {
 function getSearchedItem(item) {
     return `
         <div class="searched_item"> 
-            <div class="category">${item.category.name}</div> 
-            <div class="first_lang">${item.name}</div> 
-            <div class="second_lang">${item.translation.name}</div> 
+            <div class="category">${item.category.name}</div>
+            <div class="wrapper_status" state="hidden">
+                <div class="status save">Со</div>   
+                <div class="status loading">За</div>
+                <div class="status success">Ус</div>
+                <div class="status error">Er</div>
+            </div>
+
+            <div class="lang first_lang" contentEditable="true">${item.name}</div> 
+            <div class="lang second_lang" contentEditable="true">${item.translation.name}</div> 
         </div>
     `;
 }
@@ -96,177 +114,3 @@ function getSearchedItem(item) {
 function setState(state) {
     js.attr(js.get('.main'), 'state', state);
 } 
-
-
-// async function getSearchData() {
-//     try {  
-//         setState('loading_0');
-
-//         if (!store.pickedUser) {
-//             store.tokens = await getAllTokens(); 
-//             store.pickedUser = store.tokens[0];
-//         }
-
-//         setState('loading_1');
-
-//         let userInfo = await getUserInfo();
-
-//         setState('loading_2');
-
-//         let usersForUser = await getUsersForUser();
-
-//         setState('ok');
-
-//         renderUserInfo(userInfo);
-//         renderUsersForUser(usersForUser);
-
-//     } catch (error) {
-//         return console.log(error);
-//     }
-// }
-
-//loadAndRenderDataUsers();
-
-
-// js.get('#tokens').addEventListener('change', function(event) {
-//     store.pickedUser = event.target.value;
-// });
-
-// function setSelectOptions(tokens) {
-//     let arrayTokenHtml = [];
-//     for (let token of tokens) {
-//         let tokenHtml = `<option name="${token}">${token}</option>`;
-//         arrayTokenHtml.push(tokenHtml);
-//     }
-//     js.get('#tokens').innerHTML = arrayTokenHtml.join('');
-// }
-
-
-
-// function getAllTokens() {
-//     return new Promise ( (resolve, reject) => {
-//         req.get('http://51.75.37.65/api/debug_tokens/', (error, result) => {
-//             if (error >= 400 || !result) return reject(error);
-//             let tokens = [];
-//             for (let obj of result.results ) {
-//                 tokens.push(obj.token)
-//             }
-//             resolve(tokens);
-//         });
-//     }); 
-// }
-
-// function getUsersForUser(token=store.pickedUser) {
-//     return new Promise ( (resolve, reject) => {
-//         let settings = {
-//             url : `http://51.75.37.65/api/deck/`,
-//             token : token
-//         }
-
-//         req.get(settings, (error, result) => {
-//             if (error >= 400 || !result) return reject(error); //showError();
-//             resolve(result.data);
-//         });
-//     }); 
-// }
-
-// function renderUsersForUser(usersForUser) {
-//     let arrayHtmlUser = [];
-//     for (let userForUser of usersForUser) {
-//         arrayHtmlUser.push(getPage(userForUser));
-//     }
-//     js.get('#usersForUser').innerHTML = arrayHtmlUser.join('');
-// }
-
-// function getUserInfo(token=store.pickedUser) {
-//     return new Promise ( (resolve, reject) => {
-//         let settings = {
-//             url : `http://51.75.37.65/api/get_user_info/`,
-//             token : token
-//         }
-
-//         req.get(settings, (error, result) => {
-//             if (error >= 400 || !result) return reject(error); //showError();
-//             resolve(result.details);
-//         });
-//     }); 
-// }
-
-// function renderUserInfo(userInfo) {
-//     js.get('#userPage').innerHTML = getPage(userInfo.user);
-// }
-
-// function getPage(userInfo) {
-//     return `
-//         <div class="user_page">
-//             <div class="user_page_left">
-//                 ${getAvatar(userInfo)}
-//                 ${getPeopleOnPhoto(userInfo)}
-//             </div>
-//             <div class="user_page_right">
-
-//                 <div class="user_item user_page_first_name" >${userInfo.first_name}, ${userInfo.age}</div>
-//                 <div class="user_item user_page_" >${userInfo.sex}</div>
-
-//                 ${getDistance(userInfo)}
-
-//                 <div class="user_item user_page_occupation" >${userInfo.occupation}</div>
-
-//                 ${getLastTimeOnline(userInfo.last_time_online)}
-
-//                 <div class="user_item user_page_interests" >
-//                     ${getInterests(userInfo.interests)}
-//                 </div>
-//             </div>    
-//         </div>    
-//     `;
-
-//     function getAvatar(userInfo) {
-//         //if (userInfo.distance === undefined) return '';
-//         //
-//         return `
-//             <img class="user_page_image" src="${userInfo.avatar && userInfo.avatar.url}">
-//         `;
-//     }
-
-//     function getPeopleOnPhoto(userInfo) {
-//         if (!userInfo.avatar) return '';
-//         return `
-//             <div class="user_page_people_on_photo"> People <span>${userInfo.avatar.people_on_photo}</span></div>
-//         `;
-//     }
-
-//     function getDistance(userInfo) {
-//         if (userInfo.distance === undefined) return '';
-
-//         return `
-//             <div class="user_item"> ${userInfo.distance} </div>
-//         `;
-//     }
-
-//     function getLastTimeOnline(last_time_online) {
-//         if (!last_time_online) return '';
-//         let date = new Date(last_time_online);
-//         let options = {
-//             year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'
-//         };
-    
-//         let result = date.toLocaleDateString('en', options); 
-
-//         return `
-//             <div class="user_item user_page_last_time_online" >${result}</div>
-//         `;
-//     }
-
-//     function getInterests(interests) {
-//         let html = '';
-//         for (let interest of interests) {
-//             html += `
-//                 <div class="user_item user_page_interest">${interest.name}</div>
-//             `;
-//         }
-//         return html;
-//     }
-    
-// }
-
